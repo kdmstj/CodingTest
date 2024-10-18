33520798576f5c19f117ba1a750319c99bbaa2a6
kdmstj <rkddmstj429@gmail.com>
2024-10-18 12:53:28 +0900

    add: ATM
    - 활동 선택 문제로, 한명이 돈을 인출할 때, 다른 사람이 인출하지 못하다는 조건은 그리디의 조건인 독립성에 만족한다.
    - 최소 시간을 만족하기 위해서는 기다리는 시간을 최소화하면 된다. 이를 위해서 소요 시간을 기준으로 오름차순으로 정렬해주었다.

---
+++ BOJ/ans11399.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ans11399 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int sum = arr[0];
+        int current = arr[0];
+        for(int i = 1; i < n; i++){
+            current = current + arr[i];
+            sum += current;
+        }
+
+        System.out.println(sum);
+    }
+}
